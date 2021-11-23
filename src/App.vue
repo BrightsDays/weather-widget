6d92d98e8c382f1ef1e845fe0da49fbd896b1bb4
Viacheslav Ivanov <v.ivanov@market-soft.ru>
2021-11-23 16:17:51 +0300

    add ts to container

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import WeatherData from "@/components/WeatherData.vue";
-import CityChoose from "@/components/CityChoose.vue";
+import { defineComponent } from 'vue'
+import WeatherData from "@/components/WeatherData.vue"
+import CityChoose from "@/components/CityChoose.vue"
 
 export default defineComponent({
   name: 'App',
@@ -23,11 +23,11 @@ export default defineComponent({
     }
   },
   methods: {
-    onChangeLat(value: {type: number}) {
-      this.lat = value;
+    onChangeLat(value: number): void {
+      this.lat = value
     },
-    onChangeLon(value: {type: number}) {
-      this.lon = value;
+    onChangeLon(value: number): void {
+      this.lon = value
     }
   }
 });
@@ -35,12 +35,14 @@ export default defineComponent({
 
 <style lang="scss">
 .app {
+  position: relative;
   width: 290px;
   margin: 20px auto;
-  padding: 15px;
+  padding: 30px 15px 15px;
   max-width: 100%;
   border: 1px solid grey;
   border-radius: 8px;
+  box-sizing: border-box;
 }
 
 * {
